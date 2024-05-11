6d31c09df4d46d78ba0f1a4f7df4cf18beedda98
sebastian <sebastianipl0131@gmail.com>
2024-05-11 08:10:43 -0400

    salvador

--- clase1/LosFlorerosDeSalvador/main.cpp
+++ clase1/LosFlorerosDeSalvador/main.cpp
@@ -3,33 +3,31 @@ using namespace std;
 
 int main()
 {
-    //O(8)
-    int floreros = 100;
-    int multiplos[] = {11,22,33,44,55,66,77,88};
-
-    for (int i = 0; i < 8; i++)
+    /*
+    //O(9)
+    for (int i = 11; i <= 88; i+=11)
     {
-        if((multiplos[i]+8) % 6 == 0)
+        if((i+8) % 6 == 0)
         {
-            cout << "A Salvador le quedan: " << multiplos[i] << "Floreros por vender";
+            cout << "A Salvador le quedan: " << i << " floreros por vender" << endl;
         }
     }
-    
+    */
 
-/*
-    O(n)
-    for (int i = 0; i < floreros; ++i) {
+
+    //O(n)
+    int floreros = 100;
+    for (int i = 0; i < floreros; ++i)
+    {
         // Salvador vendio una cantidad de floreros y le quedó cierta cantidad menor de 100 y multiplo de 6
-        if ((floreros - i) % 6 == 0) {
-            // Hago la variable florerosRestantes igual a esta cantidad que cumple la condicion
-            int florerosRestantes = floreros - i;
-            
-            //Salvador vendio 8 floreros de los que le quedaban y le quedo una cantidad multiplo de 11
-            if ((florerosRestantes - 8) % 11 == 0) {
-                cout << "A salvador le quedan por vender: " << florerosRestantes - 8 << endl;
-            }
+        //luego vendio 8 floreros de los que le quedaban y le quedo una cantidad multiplo de 11
+
+        // A Salvador le queda una cantidad multiplo de 11 que al sumarle 8 es una cantidad multiplo de 6.
+        if((i % 11 == 0) && ((i+8) % 6 == 0))
+        {
+            cout << "A salvador le quedan: " << i << " floreros por vender." <<endl;
         }
     }
-*/
+
     return 0;
 }

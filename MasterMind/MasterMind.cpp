52ea1d2ec1be9c5d2ab8823965967d9e1defba10
sebastian <sebastianipl0131@gmail.com>
2024-05-11 16:53:14 -0400

    inicio master mind

--- MasterMind/MasterMind.cpp
+++ MasterMind/MasterMind.cpp
@@ -1,13 +1,46 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <cctype> // Para usar la función isdigit()
+
 using namespace std;
 
-/*
-    
-*/
-
-int main()
+string MasterMind(string input, string clave)
 {
+    string result = "";
+    bool flag = false;
 
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if(i==j && input[i]==clave[j])
+            {
+                result +='C';
+            }
+            else if(i!=j &&input[i]==clave[j]){
+                result += 'F';
+            }
+            else if(input[i]==clave[j])
+            {
+                continue;
+            }
+            
+        }
+        
+        
+    }
+    return result;
+
+}
+
+int main() {
+    string input;
+    cout << "digite: ";
+    cin >> input;
+
+    string clave = "5612";
+
+    cout << MasterMind(input, clave) << endl;
 
     return 0;
 }

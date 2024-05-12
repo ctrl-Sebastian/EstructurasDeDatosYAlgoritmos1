8ffd1b05b6bbae4d389a867d6b7e3d238b784a11
Sebastian Marrera <smarrera@cincinnatus.edu.do>
2024-05-12 13:47:24 -0400

    mastermind

--- MasterMind/MasterMind.cpp
+++ MasterMind/MasterMind.cpp
@@ -5,42 +5,33 @@
 using namespace std;
 
 // funcion MasterMind que retorna una string respuesta en funcion del input y la clave
-string MasterMind(string input, string clave)
+string MasterMind(int input, int* claveArr)
 {
+    
+    int input4 = input %10;
+    input /= 10;
+    int input3 = input %10;
+    input /= 10;
+    int input2 = input %10;
+    input /= 10;
+    int input1 = input %10;
+
+    int inputArr[4] = {input1, input2, input3, input4};
+
     // declaramos la variable result para irle agregando las letras de la respuesta
     string result = "";
 
     // loop de 4 iteraciones para iterar por cada numero de la entrada del usuario
     for (int i = 0; i < 4; i++)
     {
-        // booleano flag para verificar si se lhizo una operación.
-        bool flag = false;
-        // loop de 4 iteraciones para comparar las letras de la entrada con las letras de la clave y sus posiciones
-        for (int j = 0; j < 4; j++)
-        {
-            // si las letras están en la misma posición y son iguales se le agregua una C a la respuesta
-            // flag se hace true para indicar que se hizo una operación
-            if (i == j && input[i] == clave[j])
-            {
-                result += 'C';
-                flag = true;
-                // si se cumple, continua a la siguiente iteración.
-                continue;
-            }
-            // si no están en la misma posición y los numeros son iguales, agrega una F a la respuesta.
-            // flag se hace true para indicar que se hizo una operación
-            else if (i != j && input[i] == clave[j]) {
-                result += 'F';
-                flag = true;
-                // si se cumple, continua a la siguiente iteración.
-                continue;
-            }
+       if (inputArr[i] == claveArr[i]) {
+            result += "C";
         }
-
-        // si se termina de comparar la letra de la entrada con todas 
-        // las letras de la clave y no se agregó ni una C o F, se agrega la X
-        if (!flag) {
-            result += 'X';
+        else if (inputArr[i] == claveArr[0] || inputArr[i] == claveArr[1] || inputArr[i] == claveArr[2] || inputArr[i] == claveArr[3]) {
+            result += "F";
+        }
+        else {
+            result += "X";
         }
 
     }
@@ -49,15 +40,19 @@ string MasterMind(string input, string clave)
     return result;
 }
 
-int GeneradorNumeros()
-{
-    return 1 + (rand() % 6);
+int arrayToInt(int arr[], int size) {
+    int result = 0;
+
+    for (int i = 0; i < size; i++) {
+        result = result * 10 + arr[i];
+    }
+
+    return result;
 }
 
-
-string GeneradorClave()
+int* GeneradorClave()
 {
-    string clave = "";
+    int* clave = new int[4];
     bool DigitosUsados[6] = { false };
     srand(time(NULL));
     
@@ -66,10 +61,10 @@ string GeneradorClave()
         int num;
         do
         {
-            num = GeneradorNumeros();
+            num = 1 + (rand() % 6);
         } while (DigitosUsados[num - 1]);
 
-        clave += to_string(num);
+        clave[k] = num;
         DigitosUsados[num - 1] = true;
     }
 
@@ -78,7 +73,8 @@ string GeneradorClave()
 
 
 // Valida que el usuario no haga una entrada incorrecta
-bool ValidarEntrada(bool entradaValida, string input)
+/*
+bool ValidarEntrada(bool entradaValida, int input)
 {
     entradaValida = true;
     
@@ -112,11 +108,11 @@ bool ValidarEntrada(bool entradaValida, string input)
 
     return entradaValida;
 }
-
+*/
 int main() {
-    string clave = GeneradorClave(); // la clave es igual al string que retorna la funcion GeneradorClave()
-    string input;
-    string inputsPasados[10];
+    int* clave = GeneradorClave(); // la clave es igual al string que retorna la funcion GeneradorClave()
+    int input;
+    int inputsPasados[10];
     string respuestasPasadas[10];
     int j = 0; // variable contador que inicia en 0 e incrementara en 1 cada bucle del juego
     
@@ -130,8 +126,7 @@ int main() {
         cout << endl;
         cout << "--------------" << endl;
 
-        do
-        {
+        //do{
             entradaValida = false;
 
             // si es la primera jugada no imprimir la respuesta pasada y pedir la entrada del jugador
@@ -147,7 +142,7 @@ int main() {
             }
 
         // El programa repetira este bucle hasta que la entrada del usuario sea correcta. Se hara por lo menos una validacion, la cual ocurre al final del bucle.
-        } while (!ValidarEntrada(entradaValida, input));
+        //} while (!ValidarEntrada(entradaValida, input));
 
 
         // Agregamos el input y la respuesta generada al array de inputsPasados y de respuestasPasadas respectivamente
@@ -155,7 +150,7 @@ int main() {
         respuestasPasadas[j] = MasterMind(input, clave);
 
         // Si la respuesta es  CCCC, el jugador ganó y se le muestra su puntaje
-        if (MasterMind(input, clave) == "CCCC") {
+        if (respuestasPasadas[j] == "CCCC") {
             cout << "Felicidades! ha ganado con un puntaje de: " << i << " puntos" << endl;
             haPerdido = false;
             break;

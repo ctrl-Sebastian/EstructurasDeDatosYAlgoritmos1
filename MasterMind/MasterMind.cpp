285ab065e367e366d88812b699cce58cbe324e76
Sebastian Marrera <smarrera@cincinnatus.edu.do>
2024-05-13 10:57:15 -0400

    ahora si

--- MasterMind/MasterMind.cpp
+++ MasterMind/MasterMind.cpp
@@ -178,6 +178,8 @@ int main() {
         if (respuestasPasadas[j] == "CCCC") {
             cout << "Felicidades! ha ganado con un puntaje de: " << i << " puntos" << endl;
             haPerdido = false;
+            
+            // Espera que el usuario presione enter para salir del programa
             system("pause");
             break;
         }
@@ -188,7 +190,9 @@ int main() {
         // Si llegase a terminarse el loop y el jugador no ganó, se le muestra la clave y que perdió.
         cout << "--------------" << endl;
         cout << "Ha perdido, la clave era: " << clave << endl;
-        system("pause");
+
+        // Espera que el usuario presione enter para salir del programa
+        system("pause"); 
     }
 
     return 0;

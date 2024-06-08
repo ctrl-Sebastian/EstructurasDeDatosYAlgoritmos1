a340a39b3a843404c43af8e4e12fb2a2e87f9954
sebastian <sebastianipl0131@gmail.com>
2024-06-08 09:05:37 -0400

    factore primo

--- semana6/FactoresPrimos/FactoresPrimos.cpp
+++ semana6/FactoresPrimos/FactoresPrimos.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<limits>
 using namespace std;
 
 int main() {
@@ -8,11 +9,19 @@ int main() {
     cout << "Ingrese un numero: ";
     cin >> numero;
 
+    if (cin.fail()) {
+      cin.clear();
+      cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
+      cout << "\nOpción no válida. Por favor, ingrese un numero entre 1 y 4 "
+              "para elegir sus respectivas opciones."
+           << endl;
+    }
+
     cout << "\nFactores de " << numero << ": ";
 
     for (int i=2; numero>1; i++) {
 
-        while ((numero % i == 0) && (numero != i)) {
+        if ((numero % i == 0) && (numero != i)) {
             cout << i << "*";
             numero /= i;
         }
